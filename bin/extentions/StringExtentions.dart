0569e128adbe5ab319997fb7ccbde6fd8f5e2740
abduraimovdev <abdu778118@gmail.com>
2023-02-26 18:22:03 +0500

    Yangi Git dars ochildi !

--- bin/extentions/StringExtentions.dart
+++
@@ -1,8 +0,0 @@
-
-extension StringExtentions on String {
-  /// This is method Muhammadyusuf
-  bool validPassword() {
-    var regex = RegExp(r'(([A-Z]+)|([a-z]+)|([0-9]+)|([!@#%&]+)){8,}');
-    return regex.hasMatch(this);
-  }
-}
