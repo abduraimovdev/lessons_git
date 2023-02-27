36242ed40d85e66b994bffa85a07dc1b23a8da7c
abduraimovdev <kenzo778118@gmail.com>
2023-02-27 12:40:06 +0500

    Project created

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,58 @@
-void main() {
+import 'dart:io';
 
+void main() {
+  String s = '{}{}{}';
+  print(isValid(s));
+
+  stdout.write('N ni kiriting: ');
+  int N = int.parse(stdin.readLineSync()!);
+
+  List<int> dost_sonlar = [];
+
+  for (int i = 1; i <= N; i++) {
+    int yigindi = 0;
+    for (int j = 1; j < i; j++) {
+      if (i % j == 0) {
+        yigindi += j;
+      }
+    }
+    if (yigindi == i) {
+      dost_sonlar.add(i);
+    }
+  }
+
+  print('sonlar:');
+  for (int i = 0; i < dost_sonlar.length; i++) {
+    stdout.write('${dost_sonlar[i]} ');
+  }
+  print('');
+}
+
+bool isValid(String s) {
+  var list = s.split("");
+  for (var i in list) {
+    if (i == "{") {
+      if (list.contains("}")) {
+        list.remove(i);
+        list.remove("}");
+      } else {
+        return false;
+      }
+    } else if (i == "[") {
+      if (list.contains("]")) {
+        list.remove(i);
+        list.remove("]");
+      } else {
+        return false;
+      }
+    } else if (i == "(") {
+      if (list.contains(")")) {
+        list.remove(i);
+        list.remove(")");
+      } else {
+        return false;
+      }
+    }
+  }
+  return true;
 }
---
+++ team/bin/team.dart
@@ -0,0 +1,5 @@
+import 'package:team/team.dart' as team;
+
+void main(List<String> arguments) {
+  print('Hello world: ${team.calculate()}!');
+}
