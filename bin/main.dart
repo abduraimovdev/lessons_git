aecd4bacc6011a3be3cd3ec7ceb247d0fe89d8f5
abduraimovdev <kenzo778118@gmail.com>
2023-03-09 21:08:16 +0500

    Created

--- bin/main.dart
+++ bin/main.dart
@@ -1,7 +1,42 @@
-import 'package:lesson/color_service.dart';
+import 'package:speed_test_dart/classes/server.dart';
+import 'package:speed_test_dart/speed_test_dart.dart';
+// import 'dart:io';
+//
+// import 'package:test/expect.dart';
+//
+// void main() async {
+//   HttpClient client = HttpClient();
+//   print("Yuklanmoqda..");
+//   client.getUrl(Uri.parse("https://fluttermaster.com/wp-content/uploads/2018/08/fluttermaster.com-logo-web-header.png"))
+//       .then((HttpClientRequest request) {
+//     return request.close();
+//   }).then((HttpClientResponse response) async {
+//     response.pipe(File('./assets/images/logo_pip.png').openWrite());
+//   }).whenComplete(() => print("Yuklandi ! :)"));
+//
+//   client.close();
+// }
 
-void main() {
-  String str = "/afasfasf.red".check;
-  print("/as${str}afff.yellow".check);
-  print("/salom.red  /@3asdasak.yellows/.s s".check);
+void main() async {
+  // Create a tester instance
+  SpeedTestDart tester = SpeedTestDart();
+
+  // And a variable to store the best servers
+  List<Server> bestServersList = [];
+
+  final settings = await tester.getSettings();
+  final servers = settings.servers;
+
+  final _bestServersList = await tester.getBestServers(
+    servers: servers,
+  );
+  bestServersList = _bestServersList;
+
+  final downloadRate =
+      await tester.testDownloadSpeed(servers: bestServersList).then(print);
+
+  //Test upload speed in MB/s
+  final uploadRate = await tester.testUploadSpeed(servers: bestServersList).then(print);
+
+
 }
