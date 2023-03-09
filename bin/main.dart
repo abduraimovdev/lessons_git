e5f9feb7126d953c8812630412e42671a1d7a761
abduraimovdev <kenzo778118@gmail.com>
2023-03-09 21:09:58 +0500

    Connectivity Fully

--- bin/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:lesson/color_service.dart';
-
-void main() {
-  String str = "/afasfasf.red".check;
-  print("/as${str}afff.yellow".check);
-  print("/salom.red  /@3asdasak.yellows/.s s".check);
-}
