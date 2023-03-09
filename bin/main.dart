67f4fac5e72e81418253bfc950087cc3cccf71bf
abduraimovdev <kenzo778118@gmail.com>
2023-03-09 18:13:00 +0500

    crated

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,25 @@
-void main() {
+import 'dart:io';
+import 'package:data_connection_checker_nulls/data_connection_checker_nulls.dart';
+import 'package:test/expect.dart';
+void main()  {
+  print("Start");
+  // number(10).forEach((data) => data != 9 ? print(data) : print("odam"));
+  // print("end");
+  connectivity().forEach((data) => print(data.toString()));
+}
 
+
+Stream<int> number(int n) async* {
+  for(int i = 0; i <=n; i++) {
+    yield i;
+    await Future.delayed(Duration(seconds: 2));
+  }
+}
+
+
+Stream connectivity() async*{
+  while(true){
+    yield DataConnectionChecker().isActivelyChecking;
+    await Future.delayed(Duration(seconds: 1));
+  }
 }
