c6ee8c51d52d5f6a3d0e17dbf5ce1e1c89f07b25
abduraimovdev <kenzo778118@gmail.com>
2023-03-09 15:39:31 +0500

    Project created

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,30 @@
-void main() {
-  for(double i = 100; i > 0;  i = i / 2) {
-    print(i);
+Future<List<String>> fetchSlowCityList() async {
+  print("Loading...");
+  await Future.delayed(Duration(seconds: 2));
+  return fetchCityList();
+}
+List<String> fetchCityList() {
+  print("[Simulated Network I/O]");
+  return ['Samarqand', 'Toshkent'];
+}
+void printCities(List<String> cities) {
+  print("Cities: ");
+  for(final city in cities){
+    print("    $city");
   }
+}
+
+void main() async {
+  print("Welcome to the Cities program!");
+  // final cities = await fetchSlowCityList();
+  // printCities(cities);
+
+  final future = Future.delayed(const Duration(seconds: 3), fetchCityList);
+
+  print("The future object is actually: ${future.toString()}");
+
+  future.then((value) => print(value));
+
+  print("This happens before the futre complates");
+  future.whenComplete(() => print("sa"));
 }
